fd494b2b78367914ba46b5c7e3b753e586b868be
Jaeseok Yoon <yjaeseok@gmail.com>
2019-08-19 23:22:49 +0900

    Initial commit for RxDemo2

---
+++ app/src/main/java/com/jake/helloandroid/RxDemo2.java
@@ -0,0 +1,58 @@
+package com.jake.helloandroid;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
+
+public class RxDemo2 extends AppCompatActivity {
+
+    private static final String TAG = "RxDemo2";
+    private String greeting = "Hello From RxJava";
+    private Observable<String> myObservable;
+    private DisposableObserver<String> myObserver;
+
+    private CompositeDisposable compositeDisposable = new CompositeDisposable();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rx_demo02);
+
+        myObservable = Observable.just(greeting);
+
+        compositeDisposable.add(
+                myObservable
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribeWith(getObserver())
+        );
+    }
+
+    private DisposableObserver getObserver() {
+        myObserver = new DisposableObserver<String>() {
+            @Override
+            public void onNext(String s) {
+                Log.i(TAG, "onNext invoked " + s);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.i(TAG, "onError invoked");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.i(TAG, "onComplete invoked");
+            }
+        };
+
+        return myObserver;
+    }
+}
